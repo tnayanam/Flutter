9bc20686fa6ca122bed08e623f6e776e7b8ca682
Tanuj Nayanam <tnayanam@sessionm.com>
2020-01-16 03:44:48 -0500

    Added menu items and pass text to it and add assets and images

--- the_basics/lib/widgets/navigation_bar/navigation_bar.dart
+++ the_basics/lib/widgets/navigation_bar/navigation_bar.dart
@@ -5,6 +5,38 @@ class NavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 100,
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+            width: 150,
+            child: Image.asset('assets/logo.png'),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[_NavBar('One'), _NavBar('Two')],
+          )
+        ],
+      ),
+    );
   }
 }
+
+class _NavBar extends StatelessWidget {
+  final String title;
+  const _NavBar(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 18),
+    );
+  }
+}
+
+// row
+// row -> sizedbox
+// row -> sizedbox + row
