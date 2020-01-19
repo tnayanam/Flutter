2b877b737ddd1bc910ca0727db897ebcc5d8f3e2
Tanuj Nayanam <tnayanam@sessionm.com>
2020-01-18 19:46:51 -0500

    Added new font

--- the_basics/lib/main.dart
+++ the_basics/lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          textTheme:
+              Theme.of(context).textTheme.apply(fontFamily: 'Open Sans')),
       home: HomeView(),
     );
   }
