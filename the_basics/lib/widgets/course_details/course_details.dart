47cb0d55baf1f143f601e64e6891390f584bf370
Tanuj Nayanam <tnayanam@sessionm.com>
2020-01-19 02:19:51 -0500

    Shrink the text font based on the size of screen

--- the_basics/lib/widgets/course_details/course_details.dart
+++ the_basics/lib/widgets/course_details/course_details.dart
@@ -1,32 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class CourseDetails extends StatelessWidget {
   const CourseDetails({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 600,
-      child: Column(
-        crossAxisAlignment:
-            CrossAxisAlignment.start, // All text appears on the left side
-        mainAxisAlignment: MainAxisAlignment
-            .center, // the child appears in the center vertically
-        children: <Widget>[
-          Text(
-            'FLUTTER WEB THE \n BASICSJ', // new line
-            style: TextStyle(
-                fontWeight: FontWeight.w800, height: 0.9, fontSize: 80),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            'SDFa dafs dfasd fas we r tw rtu rth df gs fgs dfgsdfgsdfg sdfgsdfg sdfgsdf sdfgsd fgsdf gae qwer qwer qwer qwt tyr tyt gdfs  dfjh ku uio r ',
-            style: TextStyle(fontSize: 21, height: 1.7),
-          ),
-        ],
-      ),
-    );
+    return ResponsiveBuilder(builder: (context, sizeInformation) {
+      var textAlignment =
+          sizeInformation.deviceScreenType == DeviceScreenType.Desktop
+              ? TextAlign.left
+              : TextAlign.center;
+
+      double titleSize =
+          sizeInformation.deviceScreenType == DeviceScreenType.Mobile ? 50 : 80;
+
+      double descSize =
+          sizeInformation.deviceScreenType == DeviceScreenType.Mobile ? 16 : 21;
+      return Container(
+        width: 600,
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start, // All text appears on the left side
+          mainAxisAlignment: MainAxisAlignment
+              .center, // the child appears in the center vertically
+          children: <Widget>[
+            Text(
+              'FLUTTER WEB THE \n BASICSJ', // new line
+              style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  height: 0.9,
+                  fontSize: titleSize),
+              textAlign: textAlignment,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'SDFa dafs dfasd fas we r tw rtu rth df gs fgs dfgsdfgsdfg sdfgsdfg sdfgsdf sdfgsd fgsdf gae qwer qwer qwer qwt tyr tyt gdfs  dfjh ku uio r ',
+              style: TextStyle(fontSize: descSize, height: 1.7),
+              textAlign: textAlignment,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
