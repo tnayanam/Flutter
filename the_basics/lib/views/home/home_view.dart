89561ac6116b786741c73b1fb2e11bfe9996715a
Tanuj Nayanam <tnayanam@sessionm.com>
2020-01-18 20:04:46 -0500

    set max width of the sceen overall

--- the_basics/lib/views/home/home_view.dart
+++ the_basics/lib/views/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_basics/widgets/centered_view/centered_view.dart';
 import 'package:the_basics/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,11 +8,12 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[NavigationBar()],
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: CenteredView(
+          child: Column(
+            children: <Widget>[NavigationBar()],
+          ),
+        ));
   }
 }
 
